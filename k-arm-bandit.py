6614ba26359625f5035888150daa8e385d11b919
eugisbreakingthingsagain <17417539+eugisbreakingthingsagain@users.noreply.github.com>
2023-09-12 17:09:00 -0400

    Building runs dataframe

--- k-arm-bandit.py
+++ k-arm-bandit.py
@@ -16,6 +16,7 @@ class k_arm_bandit:
     def __init__(self, k=10):
         self.k = k
         self.arms = [Arm() for i in range(self.k)]
+        
         self.current_index = 0 # current index for iteration
         
     def __iter__(self):
@@ -30,14 +31,14 @@ class k_arm_bandit:
         return arm
 
 class testbed:
-    def __init__(self, epsilon = [0, 0.01, 0.1, 1], k=10, sample_size=2000):
+    def __init__(self, epsilon = [0.0, 0.01, 0.1, 1.0], k=10, run_size=2000):
         self.epsilon = epsilon
         zeroes = [0]*len(epsilon)
        
-        self.df = pd.DataFrame({'epsilon': epsilon, 'total_reward': zeroes, 'steps': zeroes, 'avg_reward': zeroes})
+        self.dfs = {'epsilon:' + str(ep) : pd.DataFrame(columns = ['Run', 'Arm', 'Reward', 'Total_Reward', 'Avg Reward']) for ep in epsilon}
         
         self.k = k
-        self.sample_size = sample_size
+        self.run_size = run_size
         
         self.bandit = k_arm_bandit(self.k)
         self.current_index = 0 # current index for iteration
@@ -55,17 +56,36 @@ class testbed:
  
 tb = testbed()
 
-# for each epsilon, perform a run
+# for each epsilon, perform a run of run_size
+
+# Run # | Explore | Arm | Reward | Total reward | Avg Reward
 
 
 # test
-for eps in tb:
-    print(eps)
+# for eps in tb:
+#     print(eps)
     
-for arm in tb.bandit:
-    print(arm.q_star)
-    print(arm.samples)
+# for arm in tb.bandit:
+#    print(arm.q_star)
+#    print(arm.samples)
     
-print(tb.df)
+print(tb.dfs)
+
+# Choose first run randomly
+run_df = pd.DataFrame({
+    'Run': range(1, tb.run_size + 1), 
+    'Random value': [0] + list(np.random.uniform(0, 1, tb.run_size-1))
+})
+
+run_df['Explore?'] = (run_df['Random value'] < tb.epsilon[2]).astype(int) 
+
+'''
+run_df['Arm'] = np.random.randint(0, tb.k - 1, tb.run_size)
+
+'''
+print(run_df)
+# do a single run: choose arm, calculate reward, update q_star for selected arm
+
+
 
 
